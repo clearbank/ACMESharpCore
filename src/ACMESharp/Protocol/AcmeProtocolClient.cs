6f865284d956aec83814e7ef85aab26d525e8941
Jamie.Peacock <jamie.peacock@clear.bank>
2026-01-28 11:51:06 +0000

    Revert "Adding logging to the profile type"
    This reverts commit 13c07c1983a19fdcb913a7cca792e4b150d51008.

--- src/ACMESharp/Protocol/AcmeProtocolClient.cs
+++ src/ACMESharp/Protocol/AcmeProtocolClient.cs
@@ -385,8 +385,6 @@ namespace ACMESharp.Protocol
             DateTime? notAfter = null,
             CancellationToken cancel = default(CancellationToken))
         {
-            _log.LogInformation($"Creating Order using preferredProfile: {preferredProfile ?? null}.");
-
             var message = new CreateOrderRequest
             {
                 Identifiers = identifiers.ToArray(),
